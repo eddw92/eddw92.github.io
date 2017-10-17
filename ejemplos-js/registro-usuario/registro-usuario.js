9b942581cf3e19583eeff60fbfa6fdf440a79651
Eddie <eddwsanchez@gmail.com>
2017-10-17 11:59:24 -0500

    Por terminar.

--- ejemplos-js/registro-usuario/registro-usuario.js
+++ ejemplos-js/registro-usuario/registro-usuario.js
@@ -1,9 +1,9 @@
 var formulario = document.getElementById('registro');
+var mensaje = document.getElementById('validacion');
 
 formulario.addEventListener('submit', function(evento){
     if(!verificaNombre() || !verificaContra() || !verificaTerminos())
     {
-        var mensaje = document.getElementById('validacion');
         mensaje.innerHTML= 'Error';
         evento.preventDefault(); //No envia nada al servidor
     }
@@ -13,14 +13,28 @@ formulario.addEventListener('submit', function(evento){
     
 });
 
+
+formulario.nombre.addEventListener('blur',function(evento){
+    verificaNombre();
+});
+
+formulario.contra.addEventListener('keyup',function(evento){
+    mensaje.innerHTML= 'Registro Exitoso';
+});
+
+
 function verificaNombre()
 {
+    var expReg = /[a-z]/i;
     
-    if(formulario.nombre.value.charCodeAt(0)>= 65 && formulario.nombre.value.charCodeAt(0)<= 90 || formulario.nombre.value.charCodeAt(0)>= 97 && formulario.nombre.value.charCodeAt(0)<= 122)
-    {
+   // if(formulario.nombre.value.charCodeAt(0)>= 65 && formulario.nombre.value.charCodeAt(0)<= 90 || formulario.nombre.value.charCodeAt(0)>= 97 && formulario.nombre.value.charCodeAt(0)<= 122)
+    if(expReg.test(formulario.nombre.value.charAt(0)))
+   {
         return true;
     } 
     else{
+        
+        mensaje.innerHTML += ' Error: Requiere una letra al comienzo del nombre de usuario. ';
         return false;
     }   
 }
@@ -58,28 +72,30 @@ function verificaContra()
             if(letras>0 && digitos>0 && simbolos>0)
             {
                 //Es fuertemente segura.
-                validacion="Es fuertemente segura.";
+                mensaje.innerHTML += " La contraseña es fuertemente segura. ";
             }
             else if(letras>0 && digitos>0)
             {
                 //Es medianamente segura.  
-                validacion="Es medianamente segura.";      
+                mensaje.innerHTML += " La contraseña es medianamente segura. ";      
             }
             else if(letras>0)
             {
                 //Es debilmente segura.
-                validacion="Es debilmente segura.";
+                mensaje.innerHTML += " La contraseña es debilmente segura. ";
             }
 
             return true;       
         }
         else
         {
+            mensaje.innerHTML += ' Error: Las contraseñas no son iguales. ';
             return false;
         }
     }
     else
     {
+        mensaje.innerHTML += ' Error: Las contraseña no cuenta con 8 caracteres minimos. ';
         return false;
     }
     
@@ -92,6 +108,7 @@ function verificaTerminos()
      return true;
    }
    else{
+    mensaje.innerHTML += ' Error: No se han aceptado los terminos y condiciones. ';
     return false;
    }  
 }
